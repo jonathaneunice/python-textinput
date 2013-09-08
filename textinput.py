2e68f420232239713ce8beac40a7946f9a6eebab
Jonathan Eunice <jonathan.eunice@gmail.com>
2013-09-07 22:06:47 -0400

    Fixed operation on Mac OS X

--- textinput.py
+++ textinput.py
@@ -4,13 +4,15 @@ import tempfile
 def textinput(content=None, editor=None):
     if not editor:
         editor = os.environ.get('EDITOR','vim')
-    tf = tempfile.NamedTemporaryFile()
+    tf = tempfile.NamedTemporaryFile(delete=False)
     if content:
         tf.write(content)
-        tf.seek(0)
+    tf.close()
     os.system("%s %s"%(editor, tf.name))
+    tf = open(tf.name, "rb")
     data = tf.read()
     tf.close()
+    os.remove(tf.name)
     if len(data) > 0 and data[-1] == "\n":
         data = data[:-1]
     return data
